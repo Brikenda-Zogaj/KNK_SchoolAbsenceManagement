6519c3ea427e19865a132afb51d2e68e8903f468
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-05-13 22:58:01 +0200

    Added 'app' package with 'maincontroller' and 'main' class, and new resources

---
+++ src/main/java/app/main.java
@@ -0,0 +1,34 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class main extends Application {
+    double x, y = 0;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("b.fxml"));
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
+
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
+    }
+}
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
-module org.example.knk_schoolabsencemanagement_gr25 {
+module org.example.projekti {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires java.sql;
+    requires javafx.base;
 
 
     opens org.example.knk_schoolabsencemanagement_gr25 to javafx.fxml;
+    opens app to javafx.fxml;
+    opens controller to javafx.fxml;
     exports org.example.knk_schoolabsencemanagement_gr25;
+    exports app to javafx.graphics;
+    exports controller;
 }
