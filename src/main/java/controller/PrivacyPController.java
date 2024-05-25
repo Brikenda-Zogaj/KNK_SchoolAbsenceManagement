5304a5669bc4c33027c31553027f792109b106a4
syzanakryeziu <syzana.kryeziu1@student.uni-pr.edu>
2024-05-25 19:46:16 +0200

    Added PrivacyPolicy

---
+++ src/main/java/controller/PrivacyPController.java
@@ -0,0 +1,23 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrivacyPController implements Initializable {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private Label titleLabel, introLabel, infoLabel, collectionLabel, purposeLabel, cookiesLabel,
+            disclosureLabel, securityLabel, rightsLabel, changesLabel, contactLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
