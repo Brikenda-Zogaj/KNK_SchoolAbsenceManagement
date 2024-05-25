6bd9e7b2dc760ddba008ed772a0648571c3f0d8b
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-05-25 23:33:00 +0200

    Added some apps

--- src/main/java/app/mungesatapp.java
+++ src/main/java/app/mungesatapp.java
@@ -4,15 +4,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class main extends Application{
+public class mungesatapp extends Application{
     @Override
     public void start(Stage stage) throws Exception{
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Fxml/b.fxml"));
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Fxml/LogIn.fxml"));
         Scene scene=new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args){
+    public static void mungesatapp(String[] args){
         launch();
     }
 }
--- src/main/java/app/studentatapp.java
+++ src/main/java/app/studentatapp.java
@@ -4,15 +4,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class main extends Application{
+public class studentatapp extends Application{
+
     @Override
     public void start(Stage stage) throws Exception{
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Fxml/b.fxml"));
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/Fxml/Nxenesit.fxml"));
         Scene scene=new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args){
+    public static void studentatapp(String[] args){
         launch();
     }
 }
